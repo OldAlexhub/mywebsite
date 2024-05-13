ff53e9b2ed60cb0e539d9160e060143182bffc26
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-05-12 23:08:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import express from "express";
-import session from "express-session";
-import MongoStore from "connect-mongo";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
@@ -15,33 +13,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const publicDirectory = path.join(__dirname, "./public");
-app.use(express.static(publicDirectory));
+const publicDirecoty = path.join(__dirname, "./public");
+app.use(express.static(publicDirecoty));
 
-// Initialize the database connection
-connectToDb()
-  .then((mongooseInstance) => {
-    app.use(
-      session({
-        secret: process.env.SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({
-          mongoUrl: process.env.MONGO_URI, // Using the connection string directly
-        }),
-      })
-    );
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: "32mb", extended: true }));
+app.use(express.urlencoded({ limit: "32mb", extended: true }));
+connectToDb();
+app.use("/", router);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-    app.use(cors());
-    app.use(helmet());
-    app.use(express.json({ limit: "32mb", extended: true }));
-    app.use(express.urlencoded({ limit: "32mb", extended: true }));
-    app.use("/", router);
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error);
-  });
