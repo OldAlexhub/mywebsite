59adda2b270779935252740e8aaa234c2120a6ee
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-05-12 23:08:33 -0400

    Update PostVisitors.js

--- controllers/PostVisitors.js
+++ controllers/PostVisitors.js
@@ -2,14 +2,14 @@ import VisitorModel from "../models/Visitors.js";
 
 const PostVisitors = async (req, res) => {
   try {
-    let visitor = await VisitorModel.findOneAndUpdate(
+    await VisitorModel.findOneAndUpdate(
       {},
       { $inc: { count: 1 } },
       { new: true, upsert: true }
     );
-    res.status(200).send("Count incremented");
+    res.send("Count incremented");
   } catch (error) {
-    res.status(500).json({ message: `Server Error!` });
+    res.status(500).send("Failed to increment count");
   }
 };
 export default PostVisitors;
