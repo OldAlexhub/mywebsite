5ea9f98289649fd9c3d4b8af02173f12c5757825
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-06-20 22:50:03 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import Show from "../controllers/Show.js";
 import ShowId from "../controllers/ShowId.js";
 import PostVisitors from "../controllers/PostVisitors.js";
 import GetVisitors from "../controllers/GetVisitors.js";
+import GetNews from "../controllers/GetNews.js";
 
 const router = Router();
 
@@ -14,5 +15,8 @@ router.get("/gets/:projectId", ShowId);
 //counter
 router.post("/counter", PostVisitors);
 router.get("/getvisitors", GetVisitors);
+
+//news
+router.get("/news", GetNews);
 
 export default router;
