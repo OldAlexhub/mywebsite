4db6273faca629893d03d2d5010b18b1fcabcb10
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-06-20 22:48:49 -0400

    Add files via upload

---
+++ controllers/GetNews.js
@@ -0,0 +1,22 @@
+import NewsAPI from "newsapi";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const newsapi = new NewsAPI(process.env.NEWS_API);
+
+const GetNews = async (req, res) => {
+  try {
+    const response = await newsapi.v2.everything({
+      q: "technology OR transportation OR data OR stock market OR ev OR data analysis OR javascript OR egypt OR Reactjs",
+      language: "en",
+      sortBy: "publishedAt",
+    });
+
+    res.status(200).json(response.articles);
+  } catch (error) {
+    res.status(500).json({ message: `Server Error` });
+  }
+};
+
+export default GetNews;
