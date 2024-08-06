85a1069fc1454056866357d5f78db2bd645338f9
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-08-05 21:26:17 -0400

    Update GetNews.js

--- controllers/GetNews.js
+++ controllers/GetNews.js
@@ -8,7 +8,7 @@ const newsapi = new NewsAPI(process.env.NEWS_API);
 const GetNews = async (req, res) => {
   try {
     const response = await newsapi.v2.everything({
-      q: "technology OR transportation OR data OR stock market OR ev OR data analysis OR javascript OR egypt OR Reactjs",
+      q: "technology OR transportation OR data OR stock market OR ev OR data analysis OR javascript OR egypt OR Data Science OR stocks OR tesla OR R programming OR Python",
       language: "en",
       sortBy: "publishedAt",
     });
