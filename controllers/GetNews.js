69b19b3cd9025967f20bfe8d835b12d57e4a562d
Mohamed Gad <140672061+OldAlexhub@users.noreply.github.com>
2024-12-10 22:19:57 -0500

    Update GetNews.js

--- controllers/GetNews.js
+++ controllers/GetNews.js
@@ -8,7 +8,7 @@ const newsapi = new NewsAPI(process.env.NEWS_API);
 const GetNews = async (req, res) => {
   try {
     const response = await newsapi.v2.everything({
-      q: "technology OR transportation OR data OR stock market OR ev OR data analysis OR javascript OR egypt OR Data Science OR stocks OR tesla OR R programming OR Python",
+      q: "Machine Learning OR OpenAI OR data OR Tesla OR EV OR Data Science OR JavaScript OR egypt OR AI OR Salesforce OR Python OR R programming OR Mathematics",
       language: "en",
       sortBy: "publishedAt",
     });
